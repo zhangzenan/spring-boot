48d020328a3d5c844b330745df6082c498426913
zhangzenan <zzn1991129@126.com>
2017-08-12 11:14:09 +0800

    springboot返回集合对象

--- springBootDemo/src/main/java/cn/itcast/controller/UserController.java
+++ springBootDemo/src/main/java/cn/itcast/controller/UserController.java
@@ -1,6 +1,10 @@
 package cn.itcast.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,8 +35,38 @@ public class UserController {
 		u.setAddress("武当山");
 		return u;
 	}
-	
-	/*public Map<String,Object> showMaps(){
-		Map<>
-	}*/
+
+	/**
+	 * 返回map集合对象
+	 */
+	@RequestMapping("maps")
+	public Map<String, Object> showMaps() {
+		Map<String, Object> maps = new HashMap<String, Object>();
+		maps.put("username", "张三");
+		maps.put("age", 19);
+
+		return maps;
+	}
+
+	/**
+	 * 返回list集合对象
+	 */
+	@RequestMapping("list")
+	public List<User> showList() {
+		// 创建一个list集合对象
+		List<User> uList = new ArrayList<User>();
+
+		// 创建user对象
+		User u1 = new User();
+		u1.setUsername("张三");
+		u1.setBirthday(new Date());
+		uList.add(u1);
+
+		User u2 = new User();
+		u2.setUsername("李四");
+		u2.setBirthday(new Date());
+		uList.add(u2);
+
+		return uList;
+	}
 }
